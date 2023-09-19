3d11c973a0c0875a24a4e7eb67fe866fc70b1c9c
Luke Hayes <lukepurplehayes@gmail.com>
2023-09-19 09:53:02 +0100

    Renamed Position to Transform.

--- main.lua
+++ main.lua
@@ -3,14 +3,14 @@ require('fw.ecs.system')
 
 local entities = {1,2,3}
 
-addPositionComponent(1, PositionComponent(300,200))
+addTransformComponent(1, TransformComponent(300,200))
 addColorComponent(   1, ColorComponent(0,0,1,1))
 
-addPositionComponent(2, PositionComponent(400,400))
+addTransformComponent(2, TransformComponent(400,400))
 addColorComponent(   2, ColorComponent(1,0,1,1))
 
 
-addPositionComponent(3, PositionComponent(600,200))
+addTransformComponent(3, TransformComponent(600,200))
 addColorComponent(   3, ColorComponent(0,1,0,1))
 addTimerComponent(   3, TimerComponent(3, true, function() end))
 
